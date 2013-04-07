340c9141ce124a47d87985eb6b04153f82cc761b
James Baicoianu <james_github@baicoianu.com>
2013-04-07 02:44:19 -0700

    Updated sector to work with new thing.generic

--- scripts/things/sector.js
+++ scripts/things/sector.js
@@ -1,90 +1,46 @@
-elation.component.add("engine.things.sector", {
-  postinit: function() {
-    var skybox = elation.utils.arrayget(this.properties, "sector.skybox");
-    var skysphere = elation.utils.arrayget(this.properties, "sector.skysphere");
-    if (skybox) {
-      this.createSky(true, skybox);
-    } else if (skysphere) {
-      this.createSky(false, skysphere);
+elation.component.add("engine.things.sector", function() {
+  this.postinit = function() {
+    this.defineProperties({
+      'fog.enabled':             { type: 'bool', default: false },
+      'fog.color':               { type: 'color', default: 0x000000 },
+      'fog.factor':              { type: 'float', default: 0.0000008 },
+      'ambient.enabled':         { type: 'bool', default: false },
+      'ambient.color':           { type: 'color', default: 0x333333 },
+      'light.enabled':           { type: 'bool', default: false },
+      'light.color':             { type: 'color', default: 0xffffff },
+      'light.position':          { type: 'vector3', default: [0,500,-500] },
+      'skybox.enabled':          { type: 'bool', default: 0 },
+      'skybox.cubePath':         { type: 'string' },
+      'terrain.enabled':         { type: 'bool', default: false },
+      'terrain.args.simple':     { type: 'bool', default: true },
+      'terrain.args.color':      { type: 'color', default: 0x998877 },
+      'terrain.args.displacementMap': { type: 'texture' },
+      'terrain.args.normalMap':  { type: 'texture' },
+      'terrain.args.map':        { type: 'texture' },
+    });
+    if (this.properties.skybox.enabled) {
+      this.setSky();
     }
-  },
-  createObject3D: function() {
+  }
+  this.createObject3D = function() {
     var obj = new THREE.Object3D();
-    var showplane = elation.utils.arrayget(this.properties, "sector.showplane");
-    var showlight = elation.utils.arrayget(this.properties, "sector.showlight");
-    
-    if (showplane == 1) {
-      var parameters = {
-        color: 0x998877,
-        shininess: 1,
-        shading: THREE.SmoothShading,
-        overdraw: true,
-      };
-      if (true) {
-        parameters.map = elation.engine.utils.materials.getTexture("/media/space/textures/dirt.jpg", [1000, 1000] );
-        parameters.normalMap = elation.engine.utils.materials.getTexture("/media/space/textures/dirt-normal.jpg", [1000, 1000] );
-        //parameters.normalScale = 0.1;
-        parameters.color = 0xffffff;
-      }
-      var repeat = 500;
-/*
-      parameters.map.repeat.set(repeat,repeat);
-      parameters.normalMap.repeat.set(repeat,repeat);
-      parameters.map.wrapS = parameters.map.wrapT = THREE.RepeatWrapping;
-      parameters.normalMap.wrapS = parameters.normalMap.wrapT = THREE.RepeatWrapping;
-*/
-
-      if (this.properties.sector.heightmap) {
-        parameters.displacementMap = elation.engine.utils.materials.getTexture(this.properties.sector.heightmap);
-        parameters.displacementScale = 1000;
-      }
-      //var material = elation.space.materials.get(parameters);
-      var material = new THREE.MeshPhongMaterial(parameters);
-      var geometry = new THREE.PlaneGeometry( 5000, 5000, 20, 20 );
-      geometry.computeFaceNormals();
-      geometry.computeVertexNormals();
-      geometry.computeTangents();
-
-      obj = new THREE.Mesh(geometry, material);
-
-      // lay plane down flat
-      var rot = new THREE.Matrix4();
-      rot.setRotationFromEuler(new THREE.Vector3(-Math.PI/2,0,0), 'XYZ');
-      obj.geometry.applyMatrix(rot);
-
-      obj.receiveShadow = true;
+    this.objects['3d'] = obj;
+    if (this.properties.light.enabled) {
+      this.spawn('light', {type: 'point', position: this.properties.light.position, color: this.properties.light.color});
     }
-    if (showlight == 1) {
-      //var light = new THREE.DirectionalLight( 0xffffff, 1, 50000);
-      var light = new THREE.PointLight( 0xffffff, 1, 500000000);
-      light.position.set(100, 500, 100);
-      //light.position.set(0, 0, 5000);
-      obj.add(light);
+    if (this.properties.ambient.enabled) {
+      this.spawn('light', {type: 'ambient', color: this.properties.ambient.color});
+    }
+    if (this.properties.terrain.enabled) {
+      this.spawn('terrain', this.properties.terrain.args);
     }
     return obj;
-  },
-  createSky: function(useskybox, path, format) {
-    //var path = "/media/space/textures/skybox/";
-    var size = 500000000;
-
-    var skyboxargs = {
-      name: 'skybox',
-      type: 'generic',
-      properties: {
-        generic: {
-          geometry: {
-            type: 'cube',
-              size: size,
-              flipSided: true
-          }
-        }
-      }
-    };
-
-    if (useskybox) {
-      if (!format) {
-        format = 'jpg';
-      }
+  }
+  this.setSky = function() {
+    var world = this.engine.systems.get('world');
+    if (!this.skytexture) {
+      var format = 'jpg';
+      var path = this.properties.skybox.cubePath;
       if (path.substr(path.length-1) != '/') {
         path += '/';
       }
@@ -95,30 +51,10 @@ elation.component.add("engine.things.sector", {
       ];
       var texturecube = THREE.ImageUtils.loadTextureCube( urls );
       texturecube.format = THREE.RGBFormat;
-      var shader = THREE.ShaderLib[ "cube" ];
-      shader.uniforms[ "tCube" ].texture = texturecube;
-
-      var material = new THREE.ShaderMaterial( {
-        fragmentShader: shader.fragmentShader,
-        vertexShader: shader.vertexShader,
-        uniforms: shader.uniforms,
-        depthWrite: false,
-        side: THREE.BackSide
-      } );
-      skyboxargs.properties.generic.geometry.type = 'cube';
-      skyboxargs.properties.generic.material = material;
-    } else {
-      skyboxargs.properties.generic.geometry.type = 'sphere';
-      skyboxargs.properties.generic.material = {
-        type: 'basic',
-        parameters: {
-          map: elation.engine.utils.materials.getTexture(path)
-        }
-      };
+      this.skytexture = texturecube;
     }
-    this.sky = elation.engine.things.generic('skybox', elation.html.create(), skyboxargs);
-    this.add(this.sky);
-  },
+    world.setSky(this.skytexture);
+  }
 }, elation.engine.things.generic);
 
 
