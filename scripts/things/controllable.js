0d3d3c0153d7610065d39e48c18c5d974ba55e0e
James Baicoianu <james_github@baicoianu.com>
2013-04-07 02:53:12 -0700

    Added things.controllable for AI and player controlled things

---
+++ scripts/things/controllable.js
@@ -0,0 +1,31 @@
+elation.component.add('engine.things.controllable', function() {
+  this.cameras = {};
+  this.controls = {};
+
+  this.initControllable = function() {
+  }
+  this.addControlContext = function(contextname, mappings, obj) {
+    var commands = {}, bindings = {};
+    for (var k in mappings) {
+      if (elation.utils.isArray(mappings[k])) {
+        var keys = mappings[k][0].split(',');
+        if (obj) {
+          commands[k] = elation.bind(obj, mappings[k][1]);
+        } else {
+          commands[k] = mappings[k][1];
+        }
+      } else {
+        commands[k] = (function(statename) { return function(ev) { this.setState(statename, ev.value); } })(k);
+        var keys = mappings[k].split(',');
+      }
+      for (i = 0; i < keys.length; i++) {
+        bindings[keys[i]] = k;
+      }
+      //this.state[k] = 0;
+    }
+
+    this.engine.systems.get('controls').addCommands(contextname, commands);
+    this.engine.systems.get('controls').addBindings(contextname, bindings);
+    this.controlcontext = contextname;
+  }
+}, elation.engine.things.generic);
