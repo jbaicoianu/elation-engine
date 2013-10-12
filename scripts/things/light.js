5186f50171c687e8e8fd4d572b36ba6bdb6389b1
James Baicoianu <james_github@baicoianu.com>
2013-10-12 11:25:08 -0700

    Improved light thing

--- scripts/things/light.js
+++ scripts/things/light.js
@@ -8,24 +8,50 @@ elation.component.add('engine.things.light', function() {
     });
   }
   this.createObject3D = function() {
-    var obj;
+    var light;
     switch (this.properties.type) {
       case 'point':
-        obj = new THREE.PointLight(this.properties.color, this.properties.intensity, this.properties.radius);
-        var helper = new THREE.PointLightHelper(obj, 1);
-        obj.add(helper);
+        this.lightobj = new THREE.PointLight(this.properties.color, this.properties.intensity, this.properties.radius);
+        //var helper = new THREE.PointLightHelper(this.lightobj, this.properties.intensity);
+        //this.lightobj.add(helper);
+        this.lightobj.castShadow = false;
+        break;
+      case 'spot':
+        this.lightobj = new THREE.SpotLight(this.properties.color, this.properties.intensity, this.properties.radius);
+        this.initShadowmap(this.lightobj);
+
+        var helper = new THREE.SpotLightHelper(this.lightobj, this.properties.intensity);
+        this.lightobj.add(helper);
         break;
       case 'directional':
-        obj = new THREE.DirectionalLight(this.properties.color, this.properties.intensity);
-        var helper = new THREE.DirectionalLightHelper(obj, 1);
-        obj.add(helper);
+        this.lightobj = new THREE.DirectionalLight(this.properties.color, this.properties.intensity);
+        this.initShadowmap(this.lightobj);
+
+        var helper = new THREE.DirectionalLightHelper(this.lightobj, this.properties.intensity);
+        this.lightobj.add(helper);
         break;
       case 'ambient':
-        obj = new THREE.AmbientLight(this.properties.color);
+        this.lightobj = new THREE.AmbientLight(this.properties.color);
         break;
-      default:
-        obj = new THREE.Object3D();
     } 
+
+    var obj = this.lightobj || new THREE.Object3D();
+    if (this.properties.render.mesh) {
+      obj = this.properties.render.mesh;
+      obj.add(this.lightobj);
+    }
     return obj;
+  }
+  this.setHex = function(color) {
+    this.lightobj.color.setHex(color);
+  }
+  this.initShadowmap = function(light) {
+    light.castShadow = true;
+    light.shadowCameraNear = 200;
+    light.shadowCameraFar = 750;
+    light.shadowCameraFov = 50;
+    light.shadowCameraVisible = false;
+    light.shadowMapWidth = 4096;
+    light.shadowMapHeight = 4096;
   }
 }, elation.engine.things.generic);
