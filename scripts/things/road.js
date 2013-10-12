6db2d7e63b18c3d046f4b298a49ad350e81a4cbb
James Baicoianu <james_github@baicoianu.com>
2013-10-12 04:10:28 -0700

    Fixed road thing type, added 3d extrusion support

--- scripts/things/road.js
+++ scripts/things/road.js
@@ -24,6 +24,7 @@ elation.component.add("engine.things.road", function(args) {
     if (this.properties.path) {
       this.createOutline();
       var mesh = this.createSurface();
+      this.objects['3d'] = mesh;
       this.createControlPoints();
       return mesh;
     }
@@ -43,7 +44,7 @@ elation.component.add("engine.things.road", function(args) {
     this.roadspline.moveTo(this.splinepoints[0].x,this.splinepoints[0].y);
     this.roadspline.splineThru(this.splinepoints.slice(1));
 
-    var thickness = .2, 
+    var thickness = .6, 
         width = elation.utils.arrayget(this.properties, "physical.width", 20),
         thickscale = 4;
     var roadpoints = [
@@ -67,9 +68,9 @@ elation.component.add("engine.things.road", function(args) {
     material = this.createMaterial({repeat: [.5, .5]});
     this.roadspline = this.createSpline();
 
-    var steps = 100;
+    var steps = Math.floor(this.roadspline.getLength() / 4);
     var frames = {tangents: [], normals: [], binormals: []};
-    var normal = new THREE.Vector3(0,1,0); // FIXME - something's upside down and I can't figure out what; this isn't the place to fix it but it's a workaround
+    var normal = new THREE.Vector3(0,1,0);
     for ( i = 0; i < steps + 1; i++ ) {
       var u = i / steps;
       var tangent = this.roadspline.getTangentAt( u ).normalize();
@@ -77,11 +78,21 @@ elation.component.add("engine.things.road", function(args) {
       frames.normals[i] = normal;
       frames.binormals[i] = tangent.clone().cross(normal);
     }
-    var geo = new THREE.ExtrudeGeometry(this.roadshape, { extrudePath: this.roadspline, frames: frames, steps: steps});
+    var uvgenerator = {
+      generateTopUV: THREE.ExtrudeGeometry.WorldUVGenerator.generateTopUV,
+      generateBottomUV: THREE.ExtrudeGeometry.WorldUVGenerator.generateBottomUV,
+      generateSideWallUV:  THREE.ExtrudeGeometry.WorldUVGenerator.generateSideWallUV,
+      foo: function(geometry, extrudedShape, wallCountour, extrudeOptions, indexA, indexB, indexC, indexD, stepIndex, stepsLength, contourIndex1, contourIndex2) {
+        var v2 = new THREE.Vector2(0,1);
+        return [v2, v2, v2, v2];
+      }
+    };
+    var geo = new THREE.ExtrudeGeometry(this.roadshape, { extrudePath: this.roadspline, frames: frames, steps: steps, closed: true, UVGenerator: uvgenerator});
     geo.computeVertexNormals();
     var mesh = new THREE.Mesh(geo, material);
     mesh.receiveShadow = true;
-    elation.utils.arrayset(this.properties, "render.outline", this.roadspline);
+    mesh.castShadow = true;
+    //elation.utils.arrayset(this.properties, "render.outline", this.roadspline);
     this.roadgeometry = geo;
     //this.removeRadarContact();
     //this.createRadarContact();
@@ -90,22 +101,32 @@ elation.component.add("engine.things.road", function(args) {
   this.createSpline = function() {
     var splinepoints = [];
     for (var k in this.properties.path) {
-      splinepoints.unshift(new THREE.Vector3(this.properties.path[k][0], this.properties.path[k][1], this.properties.path[k][2]));
+      splinepoints.push(new THREE.Vector3(this.properties.path[k][0], this.properties.path[k][1], this.properties.path[k][2]));
     }
-		var spline = new THREE.SplineCurve3(splinepoints);
+    var spline;
+    if (splinepoints[0].equals(splinepoints[splinepoints.length-1])) {
+      splinepoints.pop();
+      spline = new THREE.ClosedSplineCurve3(splinepoints);
+    } else {
+		  spline = new THREE.SplineCurve3(splinepoints);
+    }
     return spline;
   }
   this.createWireframe = function() {
     this.add(new THREE.Mesh(this.roadgeometry, new THREE.MeshBasicMaterial({color: 0xffffff, wireframe: true, opacity: 0.2, transparent: true})));
   }
   this.createControlPoints = function() {
-    var editor = elation.engine.things.pathedit(null, elation.html.create(), {
-      'type': 'pathedit',
-      'name': this.name,
-      'path': this.roadspline
-    });
-    this.add(editor);
-    elation.events.add(editor, "pathedit_change,pathedit_end", this);
+    if (!this.editor) {
+      this.editor = this.spawn('pathedit', this.name + '_edit', {
+        'pickable': true,
+        'path': this.roadspline
+      }); 
+  console.log('EDITOR', this.editor);
+      this.editor.properties.position.set(0,0,0);
+      elation.events.add(this.editor, "pathedit_change,pathedit_end", this);
+    } else {
+      this.editor.reparent(this);
+    }
   }
 }, elation.engine.things.generic);
 
