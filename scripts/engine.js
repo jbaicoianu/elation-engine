20da8d1e5c4f0ea0015ff6711d37478db2f1cb46
James Baicoianu <james_github@baicoianu.com>
2015-07-17 00:42:44 -0700

    Improved elation.client to handle more default options

--- scripts/engine.js
+++ scripts/engine.js
@@ -4,8 +4,13 @@ elation.require([
   "engine.materials",
   "engine.geometries",
   "engine.things.generic",
+  "engine.things.menu",
   "utils.math",
-  "ui.panel"
+  "ui.panel",
+  "share.share",
+  "share.targets.imgur",
+  "share.targets.dropbox",
+  "share.targets.google",
 ], function() {
   elation.requireCSS('engine.engine');
 
@@ -220,7 +225,6 @@ elation.require([
   }, elation.ui.panel);
 
   elation.component.add('engine.client', function() {
-
     this.init = function() {
       this.name = this.args.name || 'default';
 
@@ -235,13 +239,187 @@ elation.require([
       this.view = elation.engine.systems.render.view("main", elation.html.create({ tag: 'div', append: this }), { fullsize: 1, picking: true, engine: this.name, showstats: true } );
 
       this.initWorld();
-
+      this.initControls();
 
       //this.gameobj = this.engine.systems.world.children.vrcade;
       //this.gameobj.setview(this.view);
       //elation.events.add(this.loader, 'ui_loader_finish', elation.bind(this.gameobj, this.gameobj.handleLoaderFinished));
 
       engine.start();
+    }
+    this.initControls = function() {
+      this.controlstate = this.engine.systems.controls.addContext(this.name, {
+        'menu': ['keyboard_esc,gamepad_0_button_9', elation.bind(this, this.toggleMenu)],
+        'share_screenshot': ['keyboard_comma', elation.bind(this, this.shareScreenshot)],
+        'share_gif': ['keyboard_period', elation.bind(this, this.shareGif)],
+        'pointerlock': ['pointerlock', elation.bind(this, this.togglePointerLock)],
+        'vr_toggle': ['keyboard_ctrl_rightsquarebracket', elation.bind(this, this.toggleVR)],
+        'vr_calibrate': ['keyboard_ctrl_leftsquarebracket', elation.bind(this, this.calibrateVR)],
+      });
+      this.engine.systems.controls.activateContext(this.name);
+    }
+    this.showMenu = function() {
+      if (!this.menu) {
+        this.menu = this.player.camera.spawn('menu', null, { 
+          position: [0,0,-2],
+          items: [
+            { 
+              text: 'Intro',
+              callback: elation.bind(this, this.startIntro),
+              disabled: true
+            },
+            { 
+              text: 'Play',
+              callback: elation.bind(this, this.startGame)
+            },
+            { 
+              text: 'Options', 
+              callback: elation.bind(this, this.configureOptions),
+            },
+            { 
+              text: 'About',
+              callback: elation.bind(this, this.showAbout),
+            },
+/*
+            { 
+              text: 'Quit',
+              disabled: true
+            }
+*/
+          ],
+          labelcfg: {
+            size: .1,
+            lineheight: 1.5,
+            color: 0x999999,
+            hovercolor: 0x003300,
+            disabledcolor: 0x000000,
+            disabledhovercolor: 0x330000,
+          }
+        });
+      } else {
+        this.player.camera.add(this.menu);
+      }
+      this.player.disable();
+      this.menu.enable();
+      this.menuShowing = true;
+    }
+    this.hideMenu = function() {
+      if (this.menu) {
+        this.player.camera.remove(this.menu);
+        if (this.configmenu) this.configmenu.hide();
+        //if (this.loaded) {
+          this.player.enable();
+        //}
+        this.menuShowing = false;
+        this.menu.disable();
+      }
+    }
+    this.toggleMenu = function(ev) {
+      if (ev.value == 1) {
+        if (this.menuShowing) {
+          this.hideMenu();
+        } else {
+          this.showMenu();
+        }
+      }
+    }
+    this.togglePointerLock = function(ev) {
+      if (ev.value == 0) {
+        this.showMenu();
+      }
+    }
+    this.toggleFullscreen = function(ev) {
+      var view = this.view;
+      if (view && (ev.value == 1 || typeof ev.value == 'undefined')) {
+        view.toggleFullscreen();
+      }
+    }
+    this.toggleVR = function(ev) {
+      var view = this.view;
+      if (view && (ev.value == 1 || typeof ev.value == 'undefined')) {
+        var mode = (view.rendermode == 'default' ? 'oculus' : 'default');
+        view.setRenderMode(mode);
+      }
+    }
+    this.calibrateVR = function(ev) {
+      if (this.engine.systems.controls && ev.value == 1) {
+        this.engine.systems.controls.calibrateHMDs();
+      }
+    }
+    this.configureOptions = function() {
+      if (!this.configmenu) {
+        var configpanel = elation.engine.configuration({engine: this.engine, view: this.view});
+        this.configmenu = elation.ui.window({
+          append: document.body,
+          classname: this.name + '_config',
+          center: true,
+          resizable: false,
+          title: 'Configuration',
+          controls: true,
+          maximize: false,
+          minimize: false,
+          content: configpanel
+        });
+      }
+      this.configmenu.show();
+    }
+    this.startGame = function() {
+    }
+    this.showAbout = function() {
+    }
+    this.createSharePicker = function() {
+      this.sharepicker = elation.share.picker({append: document.body});
+      this.sharepicker.addShareTarget(elation.share.targets.imgur({clientid: '96d8f6e2515953a'}));
+      this.sharepicker.addShareTarget(elation.share.targets.dropbox({clientid: 'g5m5xsgqaqmf7jc'}));
+      this.sharepicker.addShareTarget(elation.share.targets.google({clientid: '374523350201-lev5al121s8u9aaq8spor3spsaugpcmd.apps.googleusercontent.com'}));
+    }
+    this.shareScreenshot = function(ev) {
+      if (typeof ev == 'undefined' || ev.value == 1) {
+        if (!this.sharepicker) {
+          this.createSharePicker();
+        }
+        var recorder = this.view.recorder;
+        recorder.captureJPG().then(elation.bind(this, function(data) {
+          var img = data.image.data;
+          this.sharepicker.share({
+            name: this.getScreenshotFilename('jpg'), 
+            type: 'image/jpeg',
+            image: img, 
+          });
+          var now = new Date().getTime();
+          console.log('finished jpg in ' + data.time.toFixed(2) + 'ms'); 
+        }));
+      }
+    }
+    this.shareGif = function(ev) {
+      if (typeof ev == 'undefined' || ev.value == 1) {
+        if (!this.sharepicker) {
+          this.createSharePicker();
+        }
+        var recorder = this.view.recorder;
+        recorder.captureGIF(512, 512, 75, 100).then(elation.bind(this, function(data) {
+          var img = data.image;
+          this.sharepicker.share({
+            name: this.getScreenshotFilename('gif'), 
+            type: 'image/gif',
+            image: img, 
+          });
+          var now = new Date().getTime();
+          console.log('finished gif in ' + data.time.toFixed(2) + 'ms'); 
+        }));
+      }
+    }
+    this.getScreenshotFilename = function(extension) {
+      if (!extension) extension = 'png';
+      var now = new Date();
+      function pad(n) {
+        if (n < 10) return '0' + n;
+        return n;
+      }
+      var date = now.getFullYear() + '-' + pad(now.getMonth() + 1) + '-' + pad(now.getDate());
+      var time = pad(now.getHours()) + ':' + pad(now.getMinutes()) + ':' + pad(now.getSeconds());
+      var filename = 'vrcade-' + date + ' ' + time + '.' + extension
+      return filename;
     }
   });
 });
