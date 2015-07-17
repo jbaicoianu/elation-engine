192672eaaa1e2369fc6e4e4fdc2ba01bdbf72ec1
bioid <bioshock@gmail.com>
2015-07-17 05:53:30 -0400

    spawn the mask on remote players

--- scripts/things/remoteplayer.js
+++ scripts/things/remoteplayer.js
@@ -1,16 +1,26 @@
 
-elation.require(['engine.things.generic'], function() {
+elation.require(['engine.things.generic', 'vrcade.maskgenerator'], function() {
   
 elation.component.add('engine.things.remoteplayer', function() {
   this.postinit = function() {
+    this.defineProperties({startposition: {type: 'vector3', default: new THREE.Vector3()}})
   };
 
   this.createObject3D = function() {
-    var geo = new THREE.BoxGeometry(1, 1, 1),
-        mat = new THREE.MeshBasicMaterial({ color: 0x00ff00 }),
+    var geo = new THREE.CylinderGeometry(1, 1, 4, 8),
+        mat = new THREE.MeshPhongMaterial({ color: 0x000000, transparent: true, opacity: 0.7 }),
         mesh = new THREE.Mesh(geo, mat);
     return mesh;
   }; 
+  
+  this.createChildren = function() {
+    this.mask = this.spawn('maskgenerator', this.properties.player_id + '_mask', {
+      'seed': this.properties.player_id.toString(),
+      'tilesize': 0.25,
+      'position': [0, 0, -1],
+      'player_id': this.properties.player_id
+    });
+  };
 }, elation.engine.things.generic);
 
 });
