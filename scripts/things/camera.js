614564f91c12a228c5b1528bbddc1b42ea1d17e0
James Baicoianu <james_github@baicoianu.com>
2015-09-14 22:03:21 -0700

    Add camerahelper to scene instead of as child of camera

--- scripts/things/camera.js
+++ scripts/things/camera.js
@@ -1,7 +1,16 @@
 elation.require(['engine.things.generic'], function() {
   elation.component.add('engine.things.camera', function() {
     this.createObject3D = function() {
-      return new THREE.PerspectiveCamera(80, 4/3, .1, 1000); // FIXME - need better default handling
+      var cam = new THREE.PerspectiveCamera(75, 4/3, .1, 1000); // FIXME - need better default handling
+      return cam;
+    }
+    this.createChildren = function() {
+      var camhelper = new THREE.CameraHelper(this.objects['3d']);
+      var scene = this.objects['3d'];
+      while (!(scene instanceof THREE.Scene)) {
+        scene = scene.parent;
+      }
+      //scene.add(camhelper);
     }
   }, elation.engine.things.generic);
 });
