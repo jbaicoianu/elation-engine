4611c18c84214b1afae6d1ce8ae4b6e9c92418ac
James Baicoianu <james_github@baicoianu.com>
2016-01-21 01:56:51 -0800

    Use three.js audio system

--- scripts/systems/sound.js
+++ scripts/systems/sound.js
@@ -1,48 +1,40 @@
-elation.require([
-  "engine.external.audiostage"
-]);
+elation.require([ ], function() {
+  elation.extend("engine.systems.sound", function(args) {
+    elation.implement(this, elation.engine.systems.system);
 
-elation.extend("engine.systems.sound", function(args) {
-  elation.implement(this, elation.engine.systems.system);
+    this.enabled = true;
 
-  this.enabled = true;
+    this.system_attach = function(ev) {
+      console.log('INIT: sound');
+      this.reallistener = new THREE.AudioListener();
 
-  this.system_attach = function(ev) {
-    console.log('INIT: sound');
-    this.stage = new AudioStage();
+      this.up = new THREE.Vector3(0,1,0);
+      this.front = new THREE.Vector3(0,0,-1);
 
-    this.up = new THREE.Vector3(0,1,0);
-    this.front = new THREE.Vector3(0,0,-1);
-
-    this.lastframepos = new THREE.Vector3();
-  }
-  this.engine_stop = function(ev) {
-    console.log('SHUTDOWN: sound');
-  }
-  this.engine_frame = function(ev) {
-    var render = this.engine.systems.render;
-    if (render.views['main'] && this.stage && this.stage.context) {
-      var camera = render.views['main'].camera;
-
-      var m = camera.matrixWorld;
-      this.front.set(-m.elements[8], -m.elements[9], -m.elements[10]);
-      this.up.set(m.elements[4], m.elements[5], m.elements[6]);
-
-
-      this.stage.context.listener.setPosition(camera.position.x, camera.position.y, camera.position.z);
-      if (elation.utils.isnumeric(this.front.x)) {
-        this.stage.context.listener.setOrientation(this.front.x, this.front.y, this.front.z, this.up.x, this.up.y, this.up.z);
-      }
-
-      /*
-      var tmpvel = camera.position.clone().sub(this.lastframepos).divideScalar(ev.data.delta);
-      this.lastframepos.copy(camera.position);
-      this.stage.context.listener.setVelocity(tmpvel.x, tmpvel.y, tmpvel.z);
-      */
+      this.lastframepos = new THREE.Vector3();
     }
-  }
-  this.mute = function(mutestate) {
-    this.enabled = (typeof mutestate == 'undefined' ? false : !mutestate);
-  }
+    this.engine_stop = function(ev) {
+      console.log('SHUTDOWN: sound');
+    }
+    this.engine_frame = function(ev) {
+    }
+    this.setActiveListener = function(listener) {
+      this.listener = listener;
+      listener.add(this.reallistener);
+    }
+    this.getActiveListener = function() {
+      return this.listener;
+    }
+    this.getRealListener = function() {
+      return this.reallistener;
+    }
+    this.mute = function(mutestate) {
+      this.enabled = (typeof mutestate == 'undefined' ? false : !mutestate);
+      if (this.enabled) {
+        this.reallistener.context.resume();
+      } else {
+        this.reallistener.context.suspend();
+      }
+    }
+  });
 });
-
