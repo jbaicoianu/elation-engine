6974faedcb808e2739d3c6bfc48ee1f3053d74d9
James Baicoianu <james_github@baicoianu.com>
2016-01-29 05:28:52 -0800

    Portal mouseover improvements, general cleanup

--- scripts/things/portal.js
+++ scripts/things/portal.js
@@ -15,19 +15,24 @@ elation.require(['engine.things.generic', 'engine.things.label'], function() {
       return root;
     }
     this.createChildren = function() {
-      this.child = this.spawn('generic', this.id + '_model', {
-        'render.collada': this.properties.render.collada, 
-        'render.meshname': this.properties.render.meshname, 
-        'position': this.properties.childposition,
-        'orientation': this.properties.childorientation.clone(),
-        'scale': this.properties.childscale.clone(),
-        persist: false,
-      });
-      elation.events.add(this.child, 'mouseover,mouseout,click', this);
+      if (this.properties.render.collada || this.properties.render.meshname) {
+        this.child = this.spawn('generic', this.id + '_model', {
+          'render.collada': this.properties.render.collada, 
+          'render.meshname': this.properties.render.meshname, 
+          'position': this.properties.childposition,
+          'orientation': this.properties.childorientation.clone(),
+          'scale': this.properties.childscale.clone(),
+          persist: false,
+        });
+        elation.events.add(this.child, 'mouseover,mouseout,click', this);
+      }
       if (this.properties.title) {
         this.label = this.spawn('label', this.id + '_label', { 
           text: this.properties.title, 
-          position: [0, 10, 0],
+          size: .1,
+          thickness: .05,
+          position: [0, 0, 0],
+          align: 'center',
           persist: false,
           color: 0x0000ee,
           emissive: 0x222266,
@@ -36,6 +41,7 @@ elation.require(['engine.things.generic', 'engine.things.label'], function() {
         elation.events.add(this.label, 'mouseover,mouseout,click', this);
       }
 
+/*
       this.light = this.spawn('light', this.id + '_light', {
         position: [0, 10, 15],
         persist: false,
@@ -45,8 +51,10 @@ elation.require(['engine.things.generic', 'engine.things.label'], function() {
         target: this.child,
         angle: Math.PI/8
       });
+*/
 
       // FIXME - dumb hack for demo!
+/*
       var collgeo = new THREE.BoxGeometry(4, 8, 4);
       var collmat = new THREE.MeshLambertMaterial({color: 0x990000, transparent: true, opacity: .5});
       var collider = new THREE.Mesh(collgeo, collmat);
@@ -54,26 +62,39 @@ elation.require(['engine.things.generic', 'engine.things.label'], function() {
       collider.position.y = 4;
       this.colliders.add(collider);
       collider.updateMatrixWorld();
+*/
     }
     this.mouseover = function(ev) {
       //this.child.properties.scale.copy(this.properties.childscale).multiplyScalar(1.2);
-      this.child.objects.dynamics.setAngularVelocity(new THREE.Vector3(0,Math.PI/4,0));
-      this.label.setEmissionColor(0x2222aa);
-      if (this.properties.url) {
-        this.light.setHex(0xccffcc);
-      } else {
-        this.light.setHex(0xff0000);
+      if (this.child) {
+        this.child.objects.dynamics.setAngularVelocity(new THREE.Vector3(0,Math.PI/4,0));
+        this.child.refresh();
+      }
+      if (this.label) {
+        this.label.setEmissionColor(0x2222aa);
+      }
+      if (this.light) {
+        if (this.properties.url) {
+          this.light.setHex(0xccffcc);
+        } else {
+          this.light.setHex(0xff0000);
+        }
       }
       this.refresh();
-      this.child.refresh();
     }
     this.mouseout = function(ev) {
       //this.child.properties.scale.copy(this.properties.childscale);
-      this.child.objects.dynamics.setAngularVelocity(new THREE.Vector3(0,0,0));
-      this.label.setEmissionColor(0x222266);
-      this.light.setHex(0x999999);
+      if (this.child) {
+        this.child.objects.dynamics.setAngularVelocity(new THREE.Vector3(0,0,0));
+        this.child.refresh();
+      }
+      if (this.label) {
+        this.label.setEmissionColor(0x222266);
+      }
+      if (this.light) {
+        this.light.setHex(0x999999);
+      }
       this.refresh();
-      this.child.refresh();
     }
     this.click = function(ev) {
       if (ev.data.distance <= 40 && this.properties.url) {
