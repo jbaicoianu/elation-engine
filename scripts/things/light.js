67e285478c9dfc27bc8e0e25b2cd4e0d94851ba5
James Baicoianu <james_github@baicoianu.com>
2016-02-05 16:38:43 -0800

    Moved light_point light_directional light_spot and light_ambient into separate files

--- scripts/things/light.js
+++ scripts/things/light.js
@@ -23,7 +23,7 @@ elation.require(['engine.things.generic'], function() {
           break;
         case 'spot':
           this.lightobj = new THREE.SpotLight(this.properties.color, this.properties.intensity, this.properties.radius, this.properties.angle);
-          this.initShadowmap(this.lightobj);
+          //this.initShadowmap(this.lightobj);
           if (this.properties.target) {
             this.lightobj.target = this.properties.target.objects['3d'];
           }
@@ -35,7 +35,7 @@ elation.require(['engine.things.generic'], function() {
         case 'directional':
           this.lightobj = new THREE.DirectionalLight(this.properties.color, this.properties.intensity);
           this.lightobj.shadowCameraVisible = false;
-          this.initShadowmap(this.lightobj);
+          //this.initShadowmap(this.lightobj);
 
           var helper = new THREE.DirectionalLightHelper(this.lightobj, this.properties.intensity);
           //this.lightobj.add(helper);
@@ -86,92 +86,4 @@ elation.require(['engine.things.generic'], function() {
       light.shadowMapHeight = 4096;
     }
   }, elation.engine.things.generic);
-  elation.component.add('engine.things.light_directional', function() {
-    this.postinit = function() {
-      this.defineProperties({
-        'color':             { type: 'color', default: 0xffffff },
-        'intensity':         { type: 'float', default: 1.0 },
-        'target':            { type: 'object' },
-      });
-    }
-    this.createObject3D = function() {
-      this.lightobj = new THREE.DirectionalLight(this.properties.color, this.properties.intensity);
-      this.lightobj.shadowCameraVisible = false;
-      if (this.properties.target) {
-        this.lightobj.target = this.properties.target.objects['3d'];
-      }
-      this.initShadowmap(this.lightobj);
-
-      var helper = new THREE.DirectionalLightHelper(this.lightobj, this.properties.intensity);
-
-      var obj = this.lightobj || new THREE.Object3D();
-      if (this.properties.render.mesh) {
-        obj = this.properties.render.mesh;
-        obj.add(this.lightobj);
-      }
-      return obj;
-    }
-  }, elation.engine.things.light);
-  elation.component.add('engine.things.light_ambient', function() {
-    this.postinit = function() {
-      this.defineProperties({
-        'color':             { type: 'color', default: 0xffffff },
-      });
-    }
-    this.createObject3D = function() {
-      this.lightobj = new THREE.AmbientLight(this.properties.color);
-      return this.lightobj;
-    }
-  }, elation.engine.things.light);
-  elation.component.add('engine.things.light_spot', function() {
-    this.postinit = function() {
-      this.defineProperties({
-        'color':             { type: 'color', default: 0xffffff },
-        'intensity':         { type: 'float', default: 1.0 },
-        'radius':            { type: 'float', default: 10000.0 },
-        'target':            { type: 'object' },
-        'angle':             { type: 'float', default: Math.PI/3 },
-        'exponent':          { type: 'float', default: 40 },
-      });
-    }
-    this.createObject3D = function() {
-      this.lightobj = new THREE.SpotLight(this.properties.color, this.properties.intensity, this.properties.radius, this.properties.angle);
-      this.initShadowmap(this.lightobj);
-      if (this.properties.target) {
-        this.lightobj.target = this.properties.target.objects['3d'];
-      }
-      this.lightobj.exponent = this.properties.exponent;
-
-      var helper = new THREE.SpotLightHelper(this.lightobj, this.properties.intensity);
-
-      var obj = this.lightobj || new THREE.Object3D();
-      if (this.properties.render.mesh) {
-        obj = this.properties.render.mesh;
-        obj.add(this.lightobj);
-      }
-      return obj;
-    }
-  }, elation.engine.things.light);
-  elation.component.add('engine.things.light_point', function() {
-    this.postinit = function() {
-      this.defineProperties({
-        'color':             { type: 'color', default: 0xffffff },
-        'intensity':         { type: 'float', default: 1.0 },
-        'radius':            { type: 'float', default: 10000.0 },
-      });
-    }
-    this.createObject3D = function() {
-      this.lightobj = new THREE.PointLight(this.properties.color, this.properties.intensity, this.properties.radius);
-      var helper = new THREE.PointLightHelper(this.lightobj, this.properties.intensity);
-      //this.lightobj.add(helper);
-      this.lightobj.castShadow = false;
-
-      var obj = this.lightobj || new THREE.Object3D();
-      if (this.properties.render.mesh) {
-        obj = this.properties.render.mesh;
-        obj.add(this.lightobj);
-      }
-      return obj;
-    }
-  }, elation.engine.things.light);
 });
