3e91b28861a0658de12a1f8dc58d10525654c2d8
James Baicoianu <james_github@baicoianu.com>
2016-04-01 01:18:57 -0700

    Body geometry for remoteplayer

--- scripts/things/remoteplayer.js
+++ scripts/things/remoteplayer.js
@@ -3,22 +3,44 @@ elation.require(['engine.things.generic', 'engine.things.maskgenerator'], functi
   
 elation.component.add('engine.things.remoteplayer', function() {
   this.postinit = function() {
-    this.defineProperties({startposition: {type: 'vector3', default: new THREE.Vector3()}})
+    this.defineProperties({
+      startposition: {type: 'vector3', default: new THREE.Vector3()},
+      player_id: {type: 'string', default: 'UnknownPlayer'},
+      player_name: {type: 'string', default: 'UnknownPlayer'},
+    });
   };
 
+/*
   this.createObject3D = function() {
     var geo = new THREE.CylinderGeometry(1, 1, 4, 8),
         mat = new THREE.MeshPhongMaterial({ color: 0x000000, transparent: true, opacity: 0.7 }),
         mesh = new THREE.Mesh(geo, mat);
     return mesh;
   }; 
+*/
   
   this.createChildren = function() {
-    this.mask = this.spawn('maskgenerator', this.properties.player_id + '_mask', {
+    this.torso = this.spawn('generic', this.properties.player_id + '_torso', {
+      'position': [0,1,0],
+    });
+    this.neck = this.torso.spawn('generic', this.properties.player_id + '_neck', {
+      'position': [0,0.6,0]
+    });
+    this.head = this.neck.spawn('generic', this.properties.player_id + '_head', {
+      'position': [0,0,0],
+    });
+    this.face = this.head.spawn('maskgenerator', this.properties.player_id + '_mask', {
       'seed': this.properties.player_id.toString(),
-      'tilesize': 0.25,
-      'position': [0, 0, -1],
+      'position': [0,0,-0.175],
+      'tilesize': 0.075,
       'player_id': this.properties.player_id
+    });
+    this.label = this.spawn('label', this.properties.player_id + '_label', {
+      size: .1,
+      align: 'center',
+      text: this.properties.player_name,
+      position: [0,2,0],
+      orientation: [0,1,0,0]
     });
   };
 }, elation.engine.things.generic);
