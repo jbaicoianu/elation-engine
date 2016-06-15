61c297db348a6b0240fac97ecfaf5e0fbf6be60a
James Baicoianu <james_github@baicoianu.com>
2016-06-15 11:52:09 -0700

    Attempt to make skybox track to camera better

--- scripts/things/skybox.js
+++ scripts/things/skybox.js
@@ -1,7 +1,8 @@
 elation.require(['engine.things.generic'], function() {
   elation.component.add('engine.things.skybox', function() {
     this.postinit = function() {
-      elation.events.add(this.engine.systems.render.views.main, 'render_view_prerender', elation.bind(this, this.updatePosition));
+      //elation.events.add(this.engine.systems.render.views.main, 'render_view_prerender', elation.bind(this, this.updatePosition));
+      elation.events.add(this.engine, 'engine_frame', elation.bind(this, this.updatePosition));
     }
     this.createObject3D = function() {
       var geo = new THREE.BoxGeometry(50, 50, 50);
@@ -29,13 +30,15 @@ elation.require(['engine.things.generic'], function() {
       this.material.uniforms[ "tCube" ].value = texture;
     }
     this.updatePosition = function(ev) {
-      var view = ev.target;
-      var camera = view.camera;
+      var view = ev.target.systems.render.views.main;
+      var camera = view.actualcamera;
       //if (camera.matrixNeedsUpdate) {
         camera.updateMatrix();
         camera.updateMatrixWorld();
       //}
       this.properties.position.setFromMatrixPosition(camera.matrixWorld);
+      this.objects['3d'].updateMatrix();
+      this.objects['3d'].updateMatrixWorld();
     }
   }, elation.engine.things.generic);
 });
