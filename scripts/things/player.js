d421b4ecca3edeae1243c9dbe980c4ecc6352d82
James Baicoianu <james_github@baicoianu.com>
2017-02-10 01:51:05 -0800

    Gamepad tilts neck instead of head

--- scripts/things/player.js
+++ scripts/things/player.js
@@ -324,9 +324,9 @@ elation.require(['engine.things.generic', 'engine.things.camera', 'engine.things
             this.objects.dynamics.setAngularVelocity(this.turnVector);
 
             if (this.headconstraint) this.headconstraint.enabled = (!this.vrdevice);// || !this.vrdevice.isPresenting);
-            this.head.objects.dynamics.setAngularVelocity(this.lookVector);
-            this.head.objects.dynamics.updateState();
-            this.head.refresh();
+            this.neck.objects.dynamics.setAngularVelocity(this.lookVector);
+            this.neck.objects.dynamics.updateState();
+            this.neck.refresh();
           }
         }
         //this.handleTargeting();
