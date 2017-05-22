bde8ec196dfcc470d79816a210234520cef9f35e
James Baicoianu <james_github@baicoianu.com>
2017-05-22 01:54:54 -0700

    Disable material editor templates

--- scripts/materials.js
+++ scripts/materials.js
@@ -1,11 +1,15 @@
 elation.require(['utils.template', 'engine.external.three.three'], function() {
   elation.requireCSS('engine.materials');
+  // FIXME - these templates are needed by the editor, but there's no reason to initialize them synchronously for every engine load
+  // Material editor isn't currently exposed anyway - we should split this out to be part of the admin system
+  /*
   if (elation.env.isBrowser) {
     elation.template.add('engine.materials.chunk', '<div class="engine_materials_chunk style_box"><h1>{chunkname}</h1><div class="engine_materials_chunk_uniforms style_box"><h2>uniforms</h2> <ul> {#uniforms} <li><h4>{name}</h4> ({type})</li> {/uniforms} </ul></div> <div class="engine_materials_chunk_vertex style_box"><h2>vertex</h2><p>{chunk.vertex_pars}</p><p>{chunk.vertex}</p></div>  <div class="engine_materials_chunk_fragment style_box"><h2>fragment</h2><p>{chunk.fragment_pars}</p><p>{chunk.fragment}</p></div></div>');
     elation.template.add('engine.materials.chunk.uniforms', '<h3>{chunkname}</h3><ul class="engine_materials_chunk_uniform"> {#uniforms} <li><h4>{name}</h4> <input value="{value}"> ({type})</li> {/uniforms} </ul>');
     elation.template.add('engine.materials.chunk.vertex', '<h3>{chunkname}</h3><p elation:component="engine.materials.editor" elation:args.chunkname="{chunkname}" elation:args.chunktype="vertex" {?params}elation:args.params=1{/params} class="engine_materials_chunk_vertex">{content}</p>');
     elation.template.add('engine.materials.chunk.fragment', '<h3>{chunkname}</h3><p elation:component="engine.materials.editor" elation:args.chunkname="{chunkname}" elation:args.chunktype="fragment" {?params}elation:args.params=1{/params} class="engine_materials_chunk_fragment">{content}</p>');
   }
+  */
 
   elation.extend("engine.materials", new function() {
 
