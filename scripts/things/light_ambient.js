b0b23cae304d007a7d17049bcd0e43c9c48d2bfb
James Baicoianu <james_github@baicoianu.com>
2017-07-30 22:54:30 -0700

    Update ambient light when color changes

--- scripts/things/light_ambient.js
+++ scripts/things/light_ambient.js
@@ -2,12 +2,17 @@ elation.require(['engine.things.light'], function() {
   elation.component.add('engine.things.light_ambient', function() {
     this.postinit = function() {
       this.defineProperties({
-        'color':             { type: 'color', default: 0xffffff },
+        'color':             { type: 'color', default: 0xffffff, set: this.updateLight },
       });
     }
     this.createObject3D = function() {
       this.lightobj = new THREE.AmbientLight(this.properties.color);
       return this.lightobj;
+    }
+    this.updateLight = function() {
+      if (this.lightobj) {
+        this.lightobj.color.copy(this.color);
+      }
     }
   }, elation.engine.things.light);
 });
