052f7a2f74c0597a90218dfbe09149eca00a7de4
James Baicoianu <james_github@baicoianu.com>
2020-07-28 23:53:37 -0700

    Added EulerDegrees class

---
+++ scripts/math.js
@@ -0,0 +1,53 @@
+function EulerDegrees(radians) {
+  this.radians = radians;
+}
+EulerDegrees.prototype = Object.assign( Object.create( THREE.Euler.prototype ), {
+  isEuler: true,
+});
+Object.defineProperties(EulerDegrees.prototype, {
+  x: {
+    enumerable: true,
+    configurable: true,
+    set: function(x) { this.radians.x = x * THREE.Math.DEG2RAD; },
+    get: function() { return this.radians.x * THREE.Math.RAD2DEG; }
+  },
+  y: {
+    enumerable: true,
+    configurable: true,
+    set: function(y) { this.radians.y = y * THREE.Math.DEG2RAD; },
+    get: function() { return this.radians.y * THREE.Math.RAD2DEG; }
+  },
+  z: {
+    enumerable: true,
+    configurable: true,
+    set: function(z) { this.radians.z = z * THREE.Math.DEG2RAD; },
+    get: function() { return this.radians.z * THREE.Math.RAD2DEG; }
+  },
+  order: {
+    enumerable: true,
+    configurable: true,
+    set: function(order) { this.radians.order = order; },
+    get: function() { return this.radians.order; }
+  },
+});
+
+EulerDegrees.prototype.set = function ( x, y, z, order ) {
+  this.x = x;
+  this.y = y;
+  this.z = z;
+  if (order) this.order = order;
+  return this;
+}
+EulerDegrees.prototype.copy = function ( euler ) {
+  this.x = euler.x;
+  this.y = euler.y;
+  this.z = euler.z;
+  this.order = euler.order;
+  return this;
+}
+EulerDegrees.prototype.clone = function () {
+  return new EulerDegrees().copy(this);
+}
+EulerDegrees.prototype.toArray = function() {
+  return [this.x, this.y, this.z, this.radians.order];
+};
