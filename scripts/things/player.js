a377e79548492052fa080be563f8016f68c1cb83
James Baicoianu <james_github@baicoianu.com>
2021-05-01 23:17:27 -0700

    Don't force manual head update

--- scripts/things/player.js
+++ scripts/things/player.js
@@ -355,7 +355,7 @@ elation.require(['engine.things.generic', 'engine.things.camera', 'engine.things
             this.objects.dynamics.setAngularVelocity(this.turnVector);
 
             this.head.objects.dynamics.setAngularVelocity(this.lookVector);
-            this.head.objects.dynamics.updateState();
+            //this.head.objects.dynamics.updateState();
             //this.neck.refresh();
           }
           if (this.headconstraint) this.headconstraint.enabled = (!this.vrdevice || !this.vrdevice.isPresenting);
