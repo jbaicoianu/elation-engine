78d5c39b37b2722fc4515d228f61501a7b4b74ac
James Baicoianu <james_github@baicoianu.com>
2021-10-14 22:20:24 -0700

    Don't force texture resize now that we're using WebGL2

--- scripts/assets.js
+++ scripts/assets.js
@@ -751,8 +751,8 @@ if (!ENV_IS_BROWSER) return;
         canvas.originalSrc = this.src;
 
         var imagemax = elation.utils.any(this.maxsize, elation.config.get('engine.assets.image.maxsize', Infinity));
-        canvas.width = (this.tex_linear ? Math.min(imagemax, this.nextHighestPowerOfTwo(image.width)) : image.width);
-        canvas.height = (this.tex_linear ? Math.min(imagemax, this.nextHighestPowerOfTwo(image.height)) : image.height);
+        canvas.width = Math.min(image.width, imagemax); //(this.tex_linear ? Math.min(imagemax, this.nextHighestPowerOfTwo(image.width)) : image.width);
+        canvas.height = Math.min(image.height, imagemax); //(this.tex_linear ? Math.min(imagemax, this.nextHighestPowerOfTwo(image.height)) : image.height);
         var ctx = canvas.getContext("2d");
         ctx.drawImage(image, 0, 0, image.width, image.height, 0, 0, canvas.width, canvas.height);
         if (this.hasalpha === null) {
